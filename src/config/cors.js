1f18bfe499d1569a012278a5aab910c43bc3f265
MarkMwendwa722 <mwendwamark415@gmail.com>
2026-07-29 13:14:37 +0300

    Update: CORS

--- src/config/cors.js
+++ src/config/cors.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "http://localhost:5174",
+  "http://localhost:5175",
   // Production frontend
   "https://liastute-website.vercel.app",
   "https://lijustore.co.ke",
